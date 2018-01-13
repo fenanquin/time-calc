0ad824a63fa707c914a1707768653f0326652364
Fernando Aquino <aquino.ftm@gmail.com>
2018-01-13 20:03:01 -0200

    adding test to verify the conversion of 1min to 60seconds

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 class TimeCalculator {
   calculate(expr) {
+    if (/min/.exec(expr)) {
+      return parseFloat(/\d/.exec(expr))*60 + 's';
+    }
+
     return expr;
   }
 }
--- index.test.js
+++ index.test.js
@@ -7,4 +7,9 @@ describe('TimeCalculator', () => {
     assert.equal('1s', calc.calculate('1s'));
     assert.equal('2s', calc.calculate('2s'));
   });
+
+  it('should convert 1min to 60s', () => {
+    let calc = new TimeCalculator('s');
+    assert.equal('60s', calc.calculate('1min'));
+  });
 });
