9bb356ccb866c0773ec01fecbaa74af43258b5bd
Fernando Aquino <aquino.ftm@gmail.com>
2018-01-14 20:17:52 -0200

    starting over with another approach

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 class TimeCalculator {
   calculate(expr) {
-    if (/min/.exec(expr)) {
-      return parseFloat(/\d/.exec(expr))*60 + 's';
-    }
+    let seconds = [];
+    if (/\+/.exec(expr)) {
+      let operands = expr.split('+');
+      operands = operands.map((operand) => operand.trim());
+      operands.forEach((operand) => {
+        let parts = operand.split(':');
+        let second = parseInt(parts[1]);
+        if (second) {
+          seconds.push(second);
+        }
+      });
 
+      let accSecond = seconds.reduce((acc, v) => acc + v);
+      let strSecond = accSecond > 9 ? accSecond : `0${accSecond}`;
+      return `0:${strSecond}`;
+    }
     return expr;
   }
 }
--- index.test.js
+++ index.test.js
@@ -2,14 +2,18 @@ const assert = require('chai').assert;
 const TimeCalculator = require('./index');
 
 describe('TimeCalculator', () => {
-  it('should convert 1s to 1s', () => {
-    let calc = new TimeCalculator('s');
-    assert.equal('1s', calc.calculate('1s'));
-    assert.equal('2s', calc.calculate('2s'));
+  it('should convert 0:01 to 0:01', () => {
+    let calc = new TimeCalculator(':');
+    assert.equal('0:01', calc.calculate('0:01'));
   });
 
-  it('should convert 1min to 60s', () => {
-    let calc = new TimeCalculator('s');
-    assert.equal('60s', calc.calculate('1min'));
+  it('should 0:01 + 0:02 be equal to 0:03', () => {
+    let calc = new TimeCalculator(':');
+    assert.equal('0:03', calc.calculate('0:01 + 0:02'));
+  });
+
+  it('should 0:09 + 0:01 be equal to 0:10', () => {
+    let calc = new TimeCalculator(':');
+    assert.equal('0:10', calc.calculate('0:09 + 0:01'));
   });
 });
